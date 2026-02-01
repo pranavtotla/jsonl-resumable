4fda254739c7cafcfdcfb466fb94fbe871a21d6a
Pranav Totla <thepranavtotla@gmail.com>
2026-02-01 22:39:57 +0530

    Bump version to 0.5.0

--- src/jsonl_resumable/__init__.py
+++ src/jsonl_resumable/__init__.py
@@ -37,7 +37,7 @@ from .exceptions import (
 from .index import JsonlIndex
 from .models import IndexMeta, JobInfo, LineInfo
 
-__version__ = "0.4.0"
+__version__ = "0.5.0"
 __all__ = [
     # Core
     "JsonlIndex",
